1973846f8414bda841dd47f3e09fb88a5386839e
Thomas Schank <DrTom@schank.ch>
2017-01-27 18:13:49 +0100

    Extract create_default_users #psh

--- spec/helpers/users.rb
+++ spec/helpers/users.rb
@@ -3,6 +3,10 @@ module Helpers
     extend self
 
     def create_default_users
+      password_digest = '$2a$06$YXyaPR7IzANgRuOx5JJTzO8THVxfeQ8wVTB.NZJzySLP6Qg5v3zhW'
+      @users = database[:users]
+      @users.insert(login: 'normin', is_admin: false, password_digest: password_digest)
+      @users.insert(login: 'admin', is_admin: true, password_digest: password_digest)
     end
 
   end
